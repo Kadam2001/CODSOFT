8353c93b5fba51a6fcd78861a57ab161aa0fd39c
nehakadam2424@gmail.com <nehakadam2424@gmail.com>
2024-09-04 00:48:07 +0530

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,19 @@
+// script.js
+
+function clearDisplay() {
+  document.getElementById('display').innerText = '';
+}
+
+function appendToDisplay(value) {
+  const display = document.getElementById('display');
+  display.innerText += value;
+}
+
+function calculateResult() {
+  const display = document.getElementById('display');
+  try {
+      display.innerText = eval(display.innerText);
+  } catch {
+      display.innerText = 'Error';
+  }
+}
